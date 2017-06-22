3db6deb131b9eef87bd46ec39cbbbc91191a4c8d
Jacob Parma <jacobparma@gmail.com>
2017-06-22 16:57:16 -0500

    S1-14 Added function components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create a new component that produces HTML
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Component's generated HTML added to the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
