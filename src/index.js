2c63c2422f3e77abbad9c771cc355a3efb4e01b6
Jacob Parma <jacobparma@gmail.com>
2017-06-27 08:52:11 -0500

    S2-21 Youtube search response

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import Config from '../config/config';
 
 import SearchBar from './components/search_bar';
 
 // Create a new component that produces HTML
 const App = () => {
+  let config = new Config();
+
+  YTSearch({key: config.API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+  })
+
   return (
     <div>
       <SearchBar />
