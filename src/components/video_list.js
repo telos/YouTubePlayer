8b3f79b27675481e2626567628cf7a5f36150a56
Jacob Parma <jacobparma@gmail.com>
2017-07-03 16:48:45 -0500

    Refactor Video List to class component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const VideoList = (props) => {
-  return (
-    <ul className="col-md-4 list-group">
-      {props.videos.length}
-    </ul>
-  );
-};
+class VideoList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul className="col-md-4 list-group">
+        {this.props.videos.length}
+      </ul>
+    );
+  }
+}
 
 export default VideoList
