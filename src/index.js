501ed8799b88a9e7bd6675f72fd06e156a2d25bd
Jacob Parma <jacobparma@gmail.com>
2017-07-05 12:00:57 -0500

    S2-29 Video Selection

--- src/index.js
+++ src/index.js
@@ -13,13 +13,17 @@ class App extends Component {
     super(props);
 
     this.state = { 
-      videos: []
+      videos: [],
+      selectedVideo: null
     };
 
     let config = new Config();
     YTSearch({key: config.API_KEY, term: 'surfboards'}, (videos) => {
       console.log('Search complete: ' + videos);
-      this.setState({ videos });
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -27,8 +31,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
